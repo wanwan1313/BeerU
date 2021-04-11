fc4d1abb9fc50f019fd86c3bc9ff68026536bfa3
wanwan <wanwan1313@gmail.com>
2021-04-11 21:44:22 +0800

    修改資料夾名稱

--- reference.php
+++ reference.php
@@ -1,4 +1,4 @@
-<?php include __DIR__ . '/php/commom/config.php' ?>
+<?php include __DIR__ . '/php/common/config.php' ?>
 
 <!-- 需要置換的變數們 -->
 <?php
@@ -7,7 +7,7 @@ $page_title = '啤女BeerU';
 
 ?>
 
-<?php include __DIR__ . '/php/commom/html-head.php' ?>
+<?php include __DIR__ . '/php/common/html-head.php' ?>
 <!-- 這裡插入要放在head的東西 -->
 <!-- 包含自己的css和js -->
 
@@ -20,7 +20,7 @@ $page_title = '啤女BeerU';
     }
 </style>
 
-<?php include __DIR__ . '/php/commom/html-body-navbar.php' ?>
+<?php include __DIR__ . '/php/common/html-body-navbar.php' ?>
 <!-- 這裡開始寫html -->
 
 <!-- 空白內容 開始寫之後請刪除以下div -->
@@ -36,12 +36,12 @@ $page_title = '啤女BeerU';
 
 
 
-<?php include __DIR__ . '/php/commom/html-body-footer.php' ?>
-<?php include __DIR__ . '/php/commom/script.php' ?>
+<?php include __DIR__ . '/php/common/html-body-footer.php' ?>
+<?php include __DIR__ . '/php/common/script.php' ?>
 <!-- 這裡開始寫jQuery或JS -->
 
 <script>
 
 </script>
 
-<?php include __DIR__ . '/php/commom/html-end.php' ?>
+<?php include __DIR__ . '/php/common/html-end.php' ?>
