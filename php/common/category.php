06e3a8b6703b88fbdad43d3f30c842c6c83f01d6
wanwan <wanwan1313@gmail.com>
2021-04-20 21:01:17 +0800

    公版修改-手機版三明治選單新增全部商品分類

---
+++ php/common/category.php
@@ -0,0 +1,174 @@
+<!-- 產品類別 -->
+<div class="product-category " style="display:none">
+
+    <div class="trigger on"><span></span></div>
+
+    <div class="menu-search d-block d-lg-none d-flex justify-content-center align-items-center">
+        <i class="fas fa-search"></i><input type="text" name="search" class="mobile-search-bar" placeholder="找啤酒">
+    </div>
+
+
+    <div class="category-title">
+        <p>商品分類</p>
+    </div>
+    <div class="category">
+        <div class="category-item category-1 hot-beer">
+            <a href="">
+                <div class="category-name0 d-flex justify-content-between">
+                    <p><i class="fas fa-crown"></i>熱門商品</p>
+                </div>
+            </a>
+        </div>
+        <div class="category-item category-4 beer-brand">
+            <div class="category-name d-flex justify-content-between">
+                <p><i class="fas fa-beer"></i>酒廠</p>
+                <div><i class="fas fa-chevron-down"></i></div>
+            </div>
+            <div class="category-sub-item-warp d-flex flex-wrap">
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">米凱勒無酒精</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">源流</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">琳德曼</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">富勒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">寇森東修道院</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">女皇爵</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">大野狼</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">迷情海岸</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">TOPA TOPA</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">摩登時代</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">布魯里</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">煙霧之城</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">富樂</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">奧斯陸</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">酵母男孩</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">八號鐵絲</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">黑暗之心</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">釀酒島主</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">唵密尼波羅</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">齊瓦雷</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">韋恩雪弗</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">艾因格</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">美人魚</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">月亮狗</a></div>
+            </div>
+            <!-- 算高度用 -->
+            <div class="category-sub-item-warp0 d-flex flex-wrap">
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">米凱勒無酒精</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">源流</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">琳德曼</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">富勒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">寇森東修道院</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">女皇爵</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">大野狼</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">迷情海岸</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">TOPA TOPA</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">摩登時代</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">布魯里</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">煙霧之城</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">富樂</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">奧斯陸</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">酵母男孩</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">八號鐵絲</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">黑暗之心</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">釀酒島主</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">唵密尼波羅</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">齊瓦雷</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">韋恩雪弗</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">艾因格</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">美人魚</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">月亮狗</a></div>
+            </div>
+        </div>
+        <div class="category-item category-2 beer-country">
+            <div class="category-name d-flex justify-content-between">
+                <p><i class="fas fa-globe-asia"></i>國家/產地</p>
+                <div><i class="fas fa-chevron-down"></i></i></div>
+            </div>
+            <div class="category-sub-item-warp d-flex flex-wrap">
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">美國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">德國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">法國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">英國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">丹麥</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">挪威</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">澳洲</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">瑞典</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">比利時</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">俄羅斯</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">紐西蘭</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">義大利</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">新加坡</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">越南</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">日本</a></div>
+            </div>
+            <!-- 算高度用 -->
+            <div class="category-sub-item-warp0 d-flex flex-wrap">
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">美國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">德國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">法國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">英國</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">丹麥</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">挪威</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">澳洲</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">瑞典</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">比利時</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">俄羅斯</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">紐西蘭</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">義大利</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">新加坡</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">越南</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">日本</a></div>
+            </div>
+        </div>
+        <div class="category-item category-3 beer-type">
+            <div class="category-name d-flex justify-content-between">
+                <p><i class="fas fa-tint"></i>酒色/類型</p>
+                <div><i class="fas fa-chevron-down"></i></div>
+            </div>
+            <div class="category-sub-item-warp d-flex flex-wrap">
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">白啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">淡色拉格</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">印度淡艾爾</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">皮爾森啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">德國小麥啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">淡色艾爾</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">勃克啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">波特啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">司陶特</a></div>
+            </div>
+            <!-- 算高度用 -->
+            <div class="category-sub-item-warp0 d-flex flex-wrap">
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">白啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">淡色拉格</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">印度淡艾爾</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">皮爾森啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">德國小麥啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">淡色艾爾</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">勃克啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">波特啤酒</a></div>
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">司陶特</a></div>
+            </div>
+        </div>
+        <div class="category-item category-5 beer-merch">
+            <div class="category-name d-flex justify-content-between">
+                <p><i class="fas fa-glass-cheers"></i>啤酒周邊</p>
+                <div><i class="fas fa-chevron-down"></i></div>
+            </div>
+            <div class="category-sub-item-warp d-flex flex-wrap justify-content-center">
+                <div class="col-6 col-lg-12 category-sub-item"><a href="">啤酒杯</a></div>
+            </div>
+            <!-- 算高度用 -->
+            <div class="category-sub-item-warp0">
+                <div class="category-sub-item"><a href="">啤酒杯</a></div>
+            </div>
+        </div>
+        <div class="category-item category-6 all-beer">
+            <a href="">
+                <div class="category-name0 d-flex justify-content-between">
+                    <p><i class="fas fa-th"></i>全部商品</p>
+                </div>
+            </a>
+        </div>
+    </div>
+
+    <div class="menu-logo"></div>
+</div>
