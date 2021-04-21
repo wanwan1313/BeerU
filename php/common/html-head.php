92389ee9ea3f29cd16910ed2b922d3a73da28903
wanwan <wanwan1313@gmail.com>
2021-04-21 20:05:06 +0800

    新增fontawesome連結

--- php/common/html-head.php
+++ php/common/html-head.php
@@ -18,6 +18,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&family=Noto+Serif+TC:wght@200;300;400;500;600;700;900&display=swap" rel="stylesheet">
     <!-- FOA -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" />
+    <!-- <link rel="stylesheet" href="<?= WEB_ROOT ?>/fontawesome/css/all.css"> -->
+
     <!-- style -->
     <link rel="stylesheet" href="<?= WEB_ROOT ?>/css/common.css">
     <link rel="stylesheet" href="<?= WEB_ROOT ?>/css/menu-footer/nav-bar.css">
