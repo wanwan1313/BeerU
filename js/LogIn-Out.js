44de1bc5c29d7e17761c2ce2bcc4c8849869960c
jin576tw <jin576tw@gmail.com>
2021-04-27 19:22:03 +0800

    login動作

--- js/LogIn-Out.js
+++ js/LogIn-Out.js
@@ -2,15 +2,40 @@ $(document).ready(function(){
 
 
 
+$('.user-login').click(function(){
+
+    $('#LogIn-Out').fadeIn(100);
+
+
+
+});
+
+// $('.log-sandbox').click(function(){
+
+//    $('#LogIn-Out').css('display','none');
+
+// })
+
 $('#LogIn-btn').click(function(){
 
 
-   $('#first-page').fadeOut();
-   $('.log-box').addClass('LogIn-box');
+   $('#first-page').fadeOut(10);
+   $('.log-box').css('width','40%').css('height','550px').css('transition','.5s');
+   $('#LogIn-page').fadeIn(100);
 
     
 })
 
+
+$('.backward-logIn h3').click(function(){
+
+    $('#LogIn-page').fadeOut(1);
+    $('#first-page').fadeIn(100);
+    $('.log-box').css('width','45%').css('height','500px').css('transition','.5s');;
+
+
+ 
+})
 
 
 })
