777566bddadf277d06cde28be70710cee3702222
jin576tw <jin576tw@gmail.com>
2021-04-29 14:41:33 +0800

    註冊頁面完成

--- js/LogIn-Out.js
+++ js/LogIn-Out.js
@@ -8,6 +8,7 @@ $('.user-login').click(function(){
     $('#first-page').fadeIn(1);
     $('.log-box').css('width','35%').css('height','500px');
     $('#LogIn-page').fadeOut(1);
+    $('#LogSign-page').fadeOut(1);
 
 });
 
@@ -23,23 +24,67 @@ $('.log-sandbox').click(function(){
 $('#LogIn-btn').click(function(){
 
    $('#first-page').fadeOut(10);
-   $('.log-box').css('width','35%').css('height','550px').css('transition','.3s');
+   $('.log-box').css('width','35%').css('height','calc(100vh - 40vh)').css('transition','.3s');
    $('#LogIn-page').fadeIn(100);
 
     
 })
 
 
-//登入頁畫面
-$('.backward-logIn h3').click(function(){
+//進入註冊畫面
+
+$('#LogOut-btn').click(function(){
+
+
+    $('#first-page').fadeOut(1);
+
+    $('.log-box').css('width','50%').css('height','calc(100vh - 25vh)').css('transition','.3s')
+
+    $('#LogSign-page').fadeIn(0.1);
+
+
+})
+
+
+// 立即註冊
+$('#LogSign-right').click(function(){
+
+    $('#first-page').fadeOut(1);
+    $('#LogIn-page').fadeOut(1);
+    $('.log-box').css('width','50%').css('height','calc(100vh - 25vh)').css('transition','.3s')
+    $('#LogSign-page').fadeIn(0.01);
+
+})
+
+
+// 馬上登入
+$('#LogIn-rightNow').click(function(){
+
+    $('#first-page').fadeOut(10);
+    $('#LogSign-page').fadeOut(1);
+    $('.log-box').css('width','35%').css('height','calc(100vh - 40vh)').css('transition','.3s');
+    $('#LogIn-page').fadeIn(100);
+ 
+     
+ })
+
+
+//回到起始畫面
+$('.backward h3').click(function(){
 
     $('#LogIn-page').fadeOut(1);
+    $('#LogSign-page').fadeOut(1);
     $('#first-page').fadeIn(100);
     $('.log-box').css('width','35%').css('height','500px').css('transition','.3s');
 
 
  
 })
+
+
+
+
+
 
 
 })
