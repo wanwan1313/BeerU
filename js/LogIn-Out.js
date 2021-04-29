6094d630180fd122318e0715cfe5eb68d63504bc
jin576tw <jin576tw@gmail.com>
2021-04-29 15:18:14 +0800

    新增退出按鈕

--- js/LogIn-Out.js
+++ js/LogIn-Out.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 //navbar登入起始畫面按鈕
 $('.user-login').click(function(){
 
-    $('#LogIn-Out').fadeIn(100);
+    $('#LogIn-Sign').fadeIn(100);
     $('#first-page').fadeIn(1);
     $('.log-box').css('width','35%').css('height','500px');
     $('#LogIn-page').fadeOut(1);
@@ -15,7 +15,13 @@ $('.user-login').click(function(){
 //按周圍區域退出
 $('.log-sandbox').click(function(){
 
-   $('#LogIn-Out').fadeOut(500);
+   $('#LogIn-Sign').fadeOut(300);
+
+})
+
+$('.cancel-quit').click(function(){
+
+   $('#LogIn-Sign').fadeOut(300);
 
 })
 
@@ -23,9 +29,9 @@ $('.log-sandbox').click(function(){
 //進入登入頁
 $('#LogIn-btn').click(function(){
 
-   $('#first-page').fadeOut(10);
-   $('.log-box').css('width','35%').css('height','calc(100vh - 40vh)').css('transition','.3s');
-   $('#LogIn-page').fadeIn(100);
+   $('#first-page').fadeOut(1);
+   $('.log-box').css('width','35%').css('height','calc(100vh - 40vh)').css('transition','.4s');
+   $('#LogIn-page').fadeIn(1000);
 
     
 })
@@ -38,9 +44,9 @@ $('#LogOut-btn').click(function(){
 
     $('#first-page').fadeOut(1);
 
-    $('.log-box').css('width','50%').css('height','calc(100vh - 25vh)').css('transition','.3s')
+    $('.log-box').css('width','50%').css('height','calc(100vh - 25vh)').css('transition','.4s')
 
-    $('#LogSign-page').fadeIn(0.1);
+    $('#LogSign-page').fadeIn(1000);
 
 
 })
@@ -51,8 +57,8 @@ $('#LogSign-right').click(function(){
 
     $('#first-page').fadeOut(1);
     $('#LogIn-page').fadeOut(1);
-    $('.log-box').css('width','50%').css('height','calc(100vh - 25vh)').css('transition','.3s')
-    $('#LogSign-page').fadeIn(0.01);
+    $('.log-box').css('width','50%').css('height','calc(100vh - 25vh)').css('transition','.4s')
+    $('#LogSign-page').fadeIn(1000);
 
 })
 
@@ -60,10 +66,10 @@ $('#LogSign-right').click(function(){
 // 馬上登入
 $('#LogIn-rightNow').click(function(){
 
-    $('#first-page').fadeOut(10);
+    $('#first-page').fadeOut(1);
     $('#LogSign-page').fadeOut(1);
-    $('.log-box').css('width','35%').css('height','calc(100vh - 40vh)').css('transition','.3s');
-    $('#LogIn-page').fadeIn(100);
+    $('.log-box').css('width','35%').css('height','calc(100vh - 40vh)').css('transition','.4s');
+    $('#LogIn-page').fadeIn(1000);
  
      
  })
@@ -75,7 +81,7 @@ $('.backward h3').click(function(){
     $('#LogIn-page').fadeOut(1);
     $('#LogSign-page').fadeOut(1);
     $('#first-page').fadeIn(100);
-    $('.log-box').css('width','35%').css('height','500px').css('transition','.3s');
+    $('.log-box').css('width','35%').css('height','500px').css('transition','.4s');
 
 
  
