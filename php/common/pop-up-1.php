98e199873cf6fb86960b8210f5c5e09e6210cd08
wanwan <wanwan1313@gmail.com>
2021-05-03 02:05:52 +0800

    改class名 & pop-up-2改結構

--- php/common/pop-up-1.php
+++ php/common/pop-up-1.php
@@ -1,4 +1,4 @@
-<section class="pop-up pop-up-1 ">
+<section class="general-pop-up pop-up-1 ">
     <div class="pop-up-box d-flex flex-column justify-content-center align-items-center">
         <div class="icon animate__animated animate__rubberBand "><i class="fas fa-times"></i></div>
         <p class="pop-up-text">請輸入正確數量</p>
--- php/common/pop-up-2.php
+++ php/common/pop-up-2.php
@@ -1,10 +1,13 @@
-<section class="pop-up pop-up-2 ">
-    <div class="pop-up-box animate__animated animate__fadeIn d-flex flex-column justify-content-center align-items-center">
+<section class="general-pop-up pop-up-2 ">
+    <div class="pop-up-box d-flex flex-column justify-content-center align-items-center">
         <div class="icon animate__animated animate__rubberBand "><i class="fas fa-times"></i></div>
         <p class="pop-up-text">請輸入正確數量</p>
-        <div class="button-wrap">
+        <div class="button-wrap-1 d-none">
             <button class="yes">是</button>
             <button class="no">否</button>
+        </div>
+        <div class="button-wrap-2 d-none">
+            <button class="ok">我知道了</button>
         </div>
     </div>
 </section>
