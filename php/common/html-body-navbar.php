0a2e4f87b8b8d6b2526a8391ed60a2c128b8f5be
jin576tw <jin576tw@gmail.com>
2021-05-03 20:48:17 +0800

    公版勿動

---
+++ php/common/html-body-navbar.php
@@ -0,0 +1,49 @@
+</head>
+
+<body>
+
+    <header class="beeru-nav-bar">
+        <div class="container">
+            <div class="row  flex-column flex-lg-row align-items-center justify-content-lg-between align-items-lg-start">
+                <div class="trigger"><span></span></div>
+                <a href="">
+                    <div class="nav-bar-logo"><img src="<?= WEB_ROOT ?>/images/logo/logo_beeru_white.svg" alt=""></div>
+                </a>
+                <ul class="nav-bar-list list-unstyled d-flex justify-content-around">
+                    <li class="nav-item"><a href="">本月主打</a></li>
+                    <li class="nav-item"><a href="">新手入門</a></li>
+                    <li class="nav-item">啤酒指南
+                        <ul class="nav-sub-menu list-unstyled">
+                            <li class="nav-sub-menu-item"><a href=""></a>啤酒地圖</a></li>
+                            <li class="nav-sub-menu-item"><a href="">餐酒搭配</a></li>
+                            <li class="nav-sub-menu-item"><a href="">風味輪</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">啤酒活動
+                        <ul class="nav-sub-menu list-unstyled">
+                            <li class="nav-sub-menu-item"><a href="">預約試飲會</a></li>
+                            <li class="nav-sub-menu-item"><a href="">募資計畫</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item d-none d-lg-block"><a href="all-product.php">全部商品</a></li>
+                </ul>
+                <ul class="nav-bar-member list-unstyled d-flex ">
+                    <input type="text" id="search" name="search" class="search-bar" placeholder="找啤酒">
+                    <li class="search"><i class="fas fa-search"></i></li>
+                    <li class="shopping-cart"><a href=""><img src="<?= WEB_ROOT ?>/images/common/icon_shopbag.svg" alt=""></a></li>
+                    <li class="user-login"><a href="javascript: "><img src="<?= WEB_ROOT ?>/images/common/icon_member.svg" alt=""></a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="header"></div>
+
+    <section class="beer-warning d-flex align-items-center">
+        <div class="container">
+            <div class="row justify-content-center align-items-center">
+                <p>禁止酒駕</p>
+                <p>酒後不開車 安全有保障</p>
+            </div>
+        </div>
+
+    </section>
