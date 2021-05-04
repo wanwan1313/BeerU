7a316bf14e101b4e1f0fb4c58fa67e5a7f046a8e
jin576tw <jin576tw@gmail.com>
2021-05-04 20:23:09 +0800

    外接DB

---
+++ php/common/__connect_db_Host.php
@@ -0,0 +1,26 @@
+<?php
+$db_host = '192.168.21.4'; //本機端或別人的IP位置(看SQL在誰那裏)
+$db_name = 'beeru'; //資料庫名稱
+$db_user = 'beeru'; //
+$db_pass = 'beeru';
+
+// data source name
+$dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $db_host, $db_name);
+
+$pdo_options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+];
+
+try {
+    $pdo = new PDO($dsn, $db_user, $db_pass, $pdo_options);
+} catch(PDOException $ex){
+    echo 'Ex:'. $ex->getMessage();
+}
+
+?>
+
+
+// 連結資料庫
+require __DIR__.'/__connect_db_Host.php';
