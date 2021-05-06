5b512cdc15c2cc42f6bdfe36e653d653ad4b79cd
wanwan <wanwan1313@gmail.com>
2021-05-06 17:11:10 +0800

    修正排序事件的網址產生

--- public/all-product.php
+++ public/all-product.php
@@ -595,7 +595,7 @@ $page_title = '啤女-精釀啤酒商品';
         page = 1
         getallproductData()
         toProductTop()
-        getURL(cate, page, hot, order)
+        getURL(cate, page, hot, order,userSearch)
         pages_wrap.css('transform', 'translateX(0px)')
 
     }
@@ -612,7 +612,7 @@ $page_title = '啤女-精釀啤酒商品';
         page = 1
         getallproductData()
         toProductTop()
-        getURL(cate, page, hot, order)
+        getURL(cate, page, hot, order, userSearch)
         pages_wrap.css('transform', 'translateX(0px)')
         $('.product-select').removeClass('on')
 
