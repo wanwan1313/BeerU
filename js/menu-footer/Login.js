fdc36e468928437368982e550bbf158824d76b8e
jin576tw <jin576tw@gmail.com>
2021-05-10 18:52:19 +0800

    隱藏登入成功pop-up

--- js/menu-footer/Login.js
+++ js/menu-footer/Login.js
@@ -12,12 +12,14 @@ function checkform_Login(){
                 console.log(data)
                 if(data.success){
                
-                    $('.pop-up-1').fadeIn(150);
-                    $('.pop-up-1 .icon').html('<i class="fas fa-check"></i>').css('background-color','var(--gold)')
-                    $('.pop-up-1 .pop-up-text').text('登入成功');
-                    $('button.ok').on('click', function () {
-                        location.reload();
-                    })
+                    // $('.pop-up-1').fadeIn(150);
+                    // $('.pop-up-1 .icon').html('<i class="fas fa-check"></i>').css('background-color','var(--gold)')
+                    // $('.pop-up-1 .pop-up-text').text('登入成功');
+                    // $('button.ok').on('click', function () {
+                        
+                    // })
+
+                    location.reload();
                     
 
 
