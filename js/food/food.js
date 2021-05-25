525f8b7cbbca02b83d19a97cd1f739ce53fe75f4
joycewwu <weniwu1121@gmail.com>
2021-05-25 17:01:02 +0800

    05/25_animation(scroll來回)修改

--- js/food/food.js
+++ js/food/food.js
@@ -133,40 +133,36 @@ $(window).scroll(function () {
 
 });
 
-// scroll-animations
-// Trigger CSS animations on scroll.
-// Detailed explanation can be found at http://www.bram.us/2013/11/20/scroll-animations/
-
-// Looking for a version that also reverses the animation when
-// elements scroll below the fold again?
-// --> Check https://codepen.io/bramus/pen/vKpjNP
+//  ------  scroll-animations  ------ //
 
 jQuery(function ($) {
-
     // Function which adds the 'animated' class to any '.animatable' in view
-    var doAnimations = function () {
-
+    let doAnimations = function () {
         // Calc current offset and get all animatables
-        var offset = $(window).scrollTop() + $(window).height(),
+        let offset = $(window).scrollTop() + $(window).height(),
             $animatables = $('.animatable');
-
         // Unbind scroll handler if we have no animatables
         if ($animatables.length == 0) {
-            $(window).off('scroll', doAnimations);
+            // $(window).off('scroll', doAnimations);
         }
-
         // Check all animatables and animate them if necessary
         $animatables.each(function (i) {
-            var $animatable = $(this);
+            let $animatable = $(this);
+            // console.log('($animatable.offset().top + $animatable.height() - 100) < offset',($animatable.offset().top + $animatable.height() - 100) < offset);
             if (($animatable.offset().top + $animatable.height() - 20) < offset) {
                 $animatable.removeClass('animatable').addClass('animated');
             }
         });
 
-    };
+        $('.animated').each(function (i) {
+            let $animatable = $(this);
 
+            if (($(this).offset().top + $(this).height() - 40) > offset) {
+                $(this).removeClass('animated').addClass('animatable');
+            }
+        });
+    };
     // Hook doAnimations on scroll, and trigger a scroll
     $(window).on('scroll', doAnimations);
     $(window).trigger('scroll');
-
 });
