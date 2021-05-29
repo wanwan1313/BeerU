91cad142bf4d3922429025ff1aa2ce889575e40b
pabi7913 <pabika790103@gmail.com>
2021-05-29 15:20:39 +0800

    20210529-更改attention on.click

--- js/map/map_attention.js
+++ js/map/map_attention.js
@@ -1,26 +1,9 @@
 // 關注功能
-let btn_attention = $('.btn_attention_be')
-let btn_attention_active = $('.btn_attention_active')
+let btn_attention = $('.buttons')
 
-// ??? 要如何呼叫php的a_arr
-// a_arr = json_encode($a_arr)
-
-// ???判斷是否已加入
-$('.map .country, .flag img').click(function () {
-    let data_cate=$(this).attr('data-cate')
-    console.log('data_cate',data_cate)
-    // console.log($a_arr)
-    // if (in_array($a['tag_sid'], $a_arr)){
-    //     console.log('hi')
-    // }
-    // if (a_arr.includes(data_cate)){
-    //     console.log('hi')
-    // }
-
-})
 // 加入
-btn_attention.click(function () {
-    let t_sid = $(this).parent().find('a').attr('data-cate')
+btn_attention.on('click','.btn_attention_be',function(){
+    let t_sid = $(this).closest('.buttons').attr('data-cate')
     console.log(t_sid)
 
     $.get('beer_map-attention-api.php', { 
@@ -37,9 +20,10 @@ btn_attention.click(function () {
     $(this).prev().removeClass('d-none')
 })
 
+
 // 取消
-btn_attention_active.click(function () {
-    let t_sid = $(this).parent().find('a').attr('data-cate')
+btn_attention.on('click','.btn_attention_active',function(){
+    let t_sid = $(this).closest('.buttons').attr('data-cate')
     console.log(t_sid)
 
     $.get('beer_map-attention-api.php', { 
