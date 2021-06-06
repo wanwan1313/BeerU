daf6bbbe120e56affecbcc2bfd9004bebdd8d42d
Wu Wan-Jung <80673905+wanwan1313@users.noreply.github.com>
2021-06-06 18:14:14 +0800

    Update __connect_db.php

--- php/common/__connect_db.php
+++ php/common/__connect_db.php
@@ -1,8 +1,8 @@
 <?php
 $db_host = 'localhost'; //本機端或別人的IP位置(看SQL在誰那裏)
 $db_name = 'beeru'; //資料庫名稱
-$db_user = 'James W'; //
-$db_pass = 'admin';
+$db_user = 'beeru'; //
+$db_pass = 'beeru';
 
 // data source name
 $dsn = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $db_host, $db_name);
