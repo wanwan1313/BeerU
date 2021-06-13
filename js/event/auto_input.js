8910dc3c54f20ecd574803528eacbbfdd42063bd
pabi7913 <pabika790103@gmail.com>
2021-06-14 02:11:21 +0800

    20210614-修改自動輸入姓名，新增清空

--- js/event/auto_input.js
+++ js/event/auto_input.js
@@ -1,10 +1,10 @@
 $('.auto_input0').click(function() {
-    const $p0_name = $('#p0_name')
+    // const $p0_name = $('#p0_name')
 
-    const $p0_mobile = $('#p0_mobile')
+    // const $p0_mobile = $('#p0_mobile')
 
 
-    $p0_name.val('任賢齊');
+    $p0_name.val('洪喆明');
     $p0_name.css('border', '2px solid var(--gold)');
     $p0_name.parent().next().removeClass('d-block').addClass('d-none').css('color', '#aaa');
     $p0_name.siblings().children().css('color', 'var(--gold)').css('display', 'inline-block');
@@ -17,7 +17,7 @@ $('.auto_input0').click(function() {
 })
 $('.auto_input1').click(function() {
 
-    $p1_name.val('九孔');
+    $p1_name.val('伍佰');
     $p1_name.css('border', '2px solid var(--gold)');
     $p1_name.parent().next().removeClass('d-block').addClass('d-none').css('color', '#aaa');
     $p1_name.siblings().children().css('color', 'var(--gold)').css('display', 'inline-block');
@@ -25,7 +25,7 @@ $('.auto_input1').click(function() {
     $p1_mobile.css('border', '2px solid var(--gold)');
     $p1_mobile.parent().next().next().removeClass('d-block').addClass('d-none').css('color', '#aaa');
     $p1_mobile.siblings().children().css('color', 'var(--gold)').css('display', 'inline-block');
-    $p2_name.val('李奧納多皮卡丘');
+    $p2_name.val('蕭花花公子帥鮭魚啊這個名字帥嗎');
     $p2_name.css('border', '2px solid var(--gold)');
     $p2_name.parent().next().removeClass('d-block').addClass('d-none').css('color', '#aaa');
     $p2_name.siblings().children().css('color', 'var(--gold)').css('display', 'inline-block');
@@ -34,28 +34,30 @@ $('.auto_input1').click(function() {
     $p2_mobile.parent().next().next().addClass('d-block').removeClass('d-none').css('color', 'var(--pink)').css('padding-left', '231px');
     $p2_mobile.siblings().children().css('display', 'none');
 })
+$('.input_reset').click(function() {
 
-// $('.join .one').before().click(function() {
-//     const $p1_name = $('#p1_name')
-
-//     const $p1_mobile = $('#p1_mobile')
-
-
-//     $p1_name.val('鄧紫棋');
-
-//     $p1_mobile.val('0912345678');
-
-
-// })
-// $('.join .two').before().click(function() {
-//     const $p2_name = $('#p2_name')
-
-//     const $p2_mobile = $('#p2_mobile')
-
-
-//     $p2_name.val('黃明志');
-
-//     $p2_mobile.val('0966320901');
-
-
-// })
+    $p0_name.val('');
+    $p0_name.css('border', 'unset');
+    $p0_name.parent().next().removeClass('d-block').addClass('d-none').css('color', '#aaa');
+    $p0_name.siblings().children().css('color', 'unset').css('display', 'none');
+    $p0_mobile.val('');
+    $p0_mobile.css('border', 'unset');
+    $p0_mobile.parent().next().removeClass('d-block').addClass('d-none').css('color', '#aaa');
+    $p0_mobile.siblings().children().css('color', 'unset').css('display', 'none');
+    $p1_name.val('');
+    $p1_name.css('border', 'unset');
+    $p1_name.parent().next().removeClass('d-block').addClass('d-none').css('color', '#aaa');
+    $p1_name.siblings().children().css('color', 'unset').css('display', 'none');
+    $p1_mobile.val('');
+    $p1_mobile.css('border', 'unset');
+    $p1_mobile.parent().next().next().removeClass('d-block').addClass('d-none').css('color', 'unset');
+    $p1_mobile.siblings().children().css('color', 'unset').css('display', 'none');
+    $p2_name.val('');
+    $p2_name.css('border', 'unset');
+    $p2_name.parent().next().removeClass('d-block').addClass('d-none').css('color', 'unset');
+    $p2_name.siblings().children().css('color', 'unset').css('display', 'none');
+    $p2_mobile.val('');
+    $p2_mobile.css('border', 'unset');
+    $p2_mobile.parent().next().next().addClass('d-none').removeClass('d-block').css('color', 'unset').css('padding-left', 'unset');
+    $p2_mobile.siblings().children().css('display', 'none');
+})
